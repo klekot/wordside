2d43cf6027275c85efa328332fb9a2fe07358e94
Igor Klekotnev <klekotnev@gmail.com>
2016-04-03 21:34:16 -0700

    Add utility for privat working w/db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,26 @@
 class HomeController < ApplicationController
+
   def index
+  end
+
+  def edit
+  	@statement = params[:statement]
+  	@apply = params[:apply]
+  	@selected_articles = Article.where("title LIKE '%\s#{@statement}'")
+  	apply_changes if @apply
+  end
+
+  private
+
+  def apply_changes
+  	@selected_articles = Article.where("title LIKE '%\s#{@apply}'")
+  	@modified_articles = []
+  	@selected_articles.each do |a|
+  		a.title = a.title.sub((' ' + @apply), '') if a.title.include?((' ' + @apply)) 
+  		a.description = a.description.prepend((@apply + ' '))
+  		a.save
+  		@modified_articles.push(a)
+  	end
+  	render 'applied'
   end
 end
