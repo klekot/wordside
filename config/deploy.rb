697453b5a773dbad0ca75490a367304fa152436b
Igor Klekotnev <klekotnev@gmail.com>
2016-04-23 12:20:11 -0700

    correct deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
 
 # Optional settings:
 set :user, 'wordside'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
+set :port, '65000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
