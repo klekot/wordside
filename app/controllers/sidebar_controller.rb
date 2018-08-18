f81205494c2fb7a9a3bc41e1ac9db41e6d776a0b
Igor Klekotnev <klekotnev@gmail.com>
2018-08-18 11:42:07 +0300

    fixes empty query

--- app/controllers/sidebar_controller.rb
+++ app/controllers/sidebar_controller.rb
@@ -12,7 +12,7 @@ class SidebarController < ApplicationController
     @translation   = search @query, advanced if @query
     @transcription = yandex(@query, 'transcription')[@query]
     respond_to do |format|
-      json = {query: @query, translation: @translation[@query], transcription: @transcription }
+      json = {query: @query, translation: @translation[@query], transcription: @transcription } if !@query.nil?
       output == 'json' ? format.json { render json: json }: format.html
     end
   end
@@ -44,11 +44,11 @@ class SidebarController < ApplicationController
   def yandex(query, format, translation = {})
     eng_alphabet = []
     ('a'..'z').each { |l| eng_alphabet.push l }
-    if eng_alphabet.include? query.chr
+    if !query.nil? && eng_alphabet.include?(query.chr)
       yandex_query = 'https://dictionary.yandex.net/api/v1/dicservice/lookup?key=' + ENV['yandex_api_key'] + '&lang=en-ru&text=' + query
       result = Nokogiri::XML(open(yandex_query))
       translation[query] = format_response_xml result, format
-    else
+    elsif !query.nil?
       flash[:alert] = 'Для этой формы допустимы только символы английского алфавита!'
     end
     translation
